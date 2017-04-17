03334194559493b7450f28d3d409924696155ce3
rocionde <rec2146@tc.columbia.edu>
2017-04-17 14:29:00 -0400

    School of Education/creating card and recycler views

---
+++ app/src/main/java/com/example/android/takehomeassignment11_rocioc/GraduateSchoolOfEducation.java
@@ -0,0 +1,22 @@
+package com.example.android.takehomeassignment11_rocioc;
+
+/**
+ * Created by ccteuser on 4/17/17.
+ */
+
+public class GraduateSchoolOfEducation {
+
+    public String name;
+    public int totalEnrollment;
+    public int yearOfFoundation;
+    public int photoId;
+
+    public GraduateSchoolOfEducation(String name, int totalEnrollment, int yearOfFoundation, int photoId) {
+        this.name = name;
+        this.totalEnrollment = totalEnrollment;
+        this.yearOfFoundation = yearOfFoundation;
+        this.photoId = photoId;
+    }
+
+
+}
--- app/src/main/java/com/example/android/takehomeassignment11_rocioc/MainActivity.java
+++ app/src/main/java/com/example/android/takehomeassignment11_rocioc/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.takehomeassignment11_rocioc;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_save:
+                Toast.makeText(this, "Changes saved", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_item_settings:
+                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
